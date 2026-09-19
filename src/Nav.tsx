b412f6037452bc285b088c33e7f7cdc3a32c3e8b
jobayerh99 <jobayerh99@gmail.com>
2026-09-19 07:30:12 +0600

    Nav bar complete but not done yet for mobile version

---
+++ src/Nav.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Logo from "./assets/logo-text.png";
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-5 container mx-auto my-5 items-center">
+        <div>
+          <img src={Logo} alt="Logo" />
+        </div>
+
+        <div>
+          <ul className="flex gap-4 items-center">
+            <li className="font-semibold text-[#DB2777]">
+              <a href="">Home</a>
+            </li>
+            <li className="simple-text">
+              <a href="">Technologies</a>
+            </li>
+            <li className="simple-text">
+              <a href="">Projects</a>
+            </li>
+            <li className="simple-text">
+              <a href="">About</a>
+            </li>
+            <li className="simple-text">
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex gap-2">
+          <button className="btn btn-neutral btn-outline rounded-full border-white">
+            Sign In
+          </button>
+
+          <button className="btn btn-active btn-secondary rounded-full text-white">
+            Sign Up
+          </button>
+        </div>
+      </div>
+        <div className="divider"></div>
+    </div>
+  );
+};
+
+export default Nav;
