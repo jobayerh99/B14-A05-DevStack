0570afbb8283da1c04a386ec2b7529b83f1a794c
jobayerh99 <jobayerh99@gmail.com>
2026-09-21 12:01:40 +0600

    Some issus fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,70 +9,72 @@ import Footer from "./Components/Footer";
 import Intro from "./Components/Intro";
 
 const fetchTechInfo = async () => {
-  const res = await fetch("/public/data.json");
+  const res = await fetch("/data.json");
   const data = await res.json();
   return data;
 };
 
-const techPromise = fetchTechInfo();
-
 function App() {
   // asid section functionality
+  const techPromise = fetchTechInfo();
 
   const [saved, setSaved] = useState<Technology[]>([]);
 
   // card section functionality
 
   const handleSavedTechnology = (singleTechnology: Technology) => {
-    const alreadySelected = saved.map((alse) => alse.id)
+    const alreadySelected = saved.map((alse) => alse.id);
 
-    if(alreadySelected.includes(singleTechnology.id)){
-      toast.error(`${singleTechnology.name} is already on your list`)
-      return
+    if (alreadySelected.includes(singleTechnology.id)) {
+      toast.error(`${singleTechnology.name} is already on your list`);
+      return;
     }
 
-    setSaved([...saved, singleTechnology])
-    toast.success(`${singleTechnology.name} is added successfully`)
+    setSaved([...saved, singleTechnology]);
+    toast.success(`${singleTechnology.name} is added successfully`);
   };
 
   // asid section functionality
 
-  const handleRemoveAll = () =>{
-    if(saved.length === 0)return
-    setSaved([])
-    toast.error("Your stack is cleared successfully")
-  }
+  const handleRemoveAll = () => {
+    if (saved.length === 0) return;
+    setSaved([]);
+    toast.error("Your stack is cleared successfully");
+  };
 
-  const handleRemoveTech = (id: string) =>{
-    const targetedTech = saved.find((tech) => tech.id === id)
-    const updatedList = saved.filter((tech) => tech.id !== id)
-    setSaved(updatedList)
-    if(targetedTech) toast.error(`${targetedTech.name} is removed successfully from your list`)
-  }
+  const handleRemoveTech = (id: string) => {
+    const targetedTech = saved.find((tech) => tech.id === id);
+    const updatedList = saved.filter((tech) => tech.id !== id);
+    setSaved(updatedList);
+    if (targetedTech)
+      toast.error(
+        `${targetedTech.name} is removed successfully from your list`,
+      );
+  };
 
   return (
     <>
       <Nav />
       <Banner />
-      <Intro/>
+      <Intro />
       <div className="container mx-auto grid grid-cols-4 gap-5">
         <div className="col-span-3">
           <Suspense fallback={<h2>Loading...</h2>}>
-          <MainLayout 
-          techPromise={techPromise} 
-          handleSavedTechnology={handleSavedTechnology}
-          saved = {saved} 
-          />
-        </Suspense>
+            <MainLayout
+              techPromise={techPromise}
+              handleSavedTechnology={handleSavedTechnology}
+              saved={saved}
+            />
+          </Suspense>
         </div>
-        <Asaid 
-        technologys = {saved}
-        handleRemoveAll = {handleRemoveAll}
-        handleRemoveTech = {handleRemoveTech}
+        <Asaid
+          technologys={saved}
+          handleRemoveAll={handleRemoveAll}
+          handleRemoveTech={handleRemoveTech}
         />
       </div>
 
-      <Footer/>
+      <Footer />
 
       <ToastContainer />
     </>
